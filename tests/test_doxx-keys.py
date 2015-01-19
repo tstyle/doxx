bcb7935851ff5129bb559b10a7c5d6a4d5f28085
Chris Simpkins <git.simpkins@gmail.com>
2015-01-18 19:56:58 -0500

    updated tests, new test files

--- tests/test_doxx-keys.py
+++ tests/test_doxx-keys.py
@@ -11,7 +11,11 @@ class DoxxASCIIKeysTests(unittest.TestCase):
     def setUp(self):
         self.main_test_dir = os.getcwd()
         self.good_key_path = "ascii_singletempl_key.yaml"  # need to chdir into keys directory to use this path
+        self.good_key_path_multitempl = "ascii_multitempl_key.yaml"
+        self.good_key_path_remtempl = "ascii_singletempl_rem_key.yaml"
         self.good_key_path_outside = "keys/ascii_singletempl_key.yaml"
+        self.good_key_path_multitempl_outside = "keys/ascii_multitempl_key.yaml"
+        self.good_key_path_remtempl_outside = "keys/ascii_singletempl_rem_key.yaml"
         self.bad_key_path = "boguskey.yaml"
 
     def tearDown(self):
@@ -84,4 +88,94 @@ class DoxxASCIIKeysTests(unittest.TestCase):
         self.assertFalse(key.multi_template_key) 
         
     
+    ## Multi-template ASCII tests
+    
+    # test instance attributes on construction
+    def test_doxxkey_multiascii_key_read_successful(self):
+        os.chdir('keys')
+        key = DoxxKey(self.good_key_path_multitempl)
+
+        os.chdir(self.main_test_dir)
+
+    def test_doxxkey_multiascii_metadata_template_attr(self):
+        os.chdir('keys')  # switch to keys directory to test file load from inside the directory
+        key = DoxxKey(self.good_key_path_multitempl)
+        self.assertTrue('templates' in key.meta_data.keys())  # assert that 'template' is one of keys in the meta_data attribute
+        meta_keys = key.meta_data['templates']
+        self.assertEqual('test1.doxt', meta_keys[0])  # assert that the multiple template path strings are defined relative to the key file when key file in CWD
+        self.assertEqual('test2.doxt', meta_keys[1])
+        self.assertEqual('test3.doxt', meta_keys[2])
+        
+        os.chdir(self.main_test_dir)
+
+    def test_doxxkey_multiascii_metadata_template_attr_diffdir(self):
+        key = DoxxKey(self.good_key_path_multitempl_outside)
+        self.assertTrue('templates' in key.meta_data.keys())  # assert that 'template' is one of keys in the meta_data attribute
+        self.assertEqual('keys/test1.doxt', key.meta_data['templates'][0])  # assert that the multiple template paths remain relative to the key directory when key file outside CWD
+        self.assertEqual('keys/test2.doxt', key.meta_data['templates'][1])
+        self.assertEqual('keys/test3.doxt', key.meta_data['templates'][2])
+
+    def test_doxxkey_multiascii_keydata_string_attr(self):  # test a single word string value in the key data
+        key = DoxxKey(self.good_key_path_multitempl_outside)
+        self.assertTrue('string' in key.key_data.keys())
+        self.assertEqual('Chris', key.key_data['string'])
+
+    def test_doxxkey_multiascii_keydata_stringspaces_attr(self):  # test multi-word string value that includes spaces in the key data
+        key = DoxxKey(self.good_key_path_multitempl_outside)
+        self.assertTrue('string-spaces' in key.key_data.keys())
+        self.assertEqual('more cowbell', key.key_data['string-spaces'])
+
+    def test_doxxkey_multiascii_keydata_number_attr(self):  # test an integer value in the key data
+        key = DoxxKey(self.good_key_path_multitempl_outside)
+        self.assertTrue('number' in key.key_data.keys())
+        self.assertEqual('2015', key.key_data['number'])
+
+    def test_doxxkey_multiascii_keydata_multilinestring_attr(self):  # test how multiline YAML strings are handled (the newlines are folded, becomes string without newlines)
+        key = DoxxKey(self.good_key_path_multitempl_outside)
+        self.assertTrue('multiline' in key.key_data.keys())
+        self.assertEqual("This is a multiple line string with more input", key.key_data['multiline'])  # assert that multiline strings fold newlines
+
+    def test_doxxkey_multiascii_keydata_date_attr(self):  # test whether date converted to a string
+        key = DoxxKey(self.good_key_path_multitempl_outside)
+        self.assertTrue('date' in key.key_data.keys())
+        self.assertEqual('10/10/2010', key.key_data['date'])
+
+    def test_doxxkey_multiascii_keydata_colons_attr(self):  # test that string that contains colons parses without issues
+        key = DoxxKey(self.good_key_path_multitempl_outside)
+        self.assertTrue('colons' in key.key_data.keys())
+        self.assertEqual('contains:colons', key.key_data['colons'])
+
+    def test_doxxkey_multiascii_keydata_nonalphchars_attr(self):  # test non-alphanumeric characters to confirm no parsing issues
+        key = DoxxKey(self.good_key_path_multitempl_outside)
+        self.assertTrue('nonalph-chars' in key.key_data.keys())
+        self.assertEqual('!@#$%^&*()-_=+~``<>,./?:;', key.key_data['nonalph-chars'])
+
+    def test_doxxkey_multiascii_keydata_url_attr(self):  # test that URL symbols do not cause issues
+        key = DoxxKey(self.good_key_path_multitempl_outside)
+        self.assertTrue('url' in key.key_data.keys())
+        self.assertEqual('http://test.com/dir/file.txt', key.key_data['url'])
+
+    def test_doxxkey_multiascii_attr_multitemp_false(self):  # assert that the multitemplate attribute is set to True for a multitemplate key file
+        key = DoxxKey(self.good_key_path_multitempl_outside)
+        self.assertTrue(key.multi_template_key)
+    
+    ## Single remote template test
+    # test instance attributes on construction
+    def test_doxxkey_remascii_key_read_successful(self):
+        os.chdir('keys')
+        key = DoxxKey(self.good_key_path_remtempl)
+
+        os.chdir(self.main_test_dir)
+        
+    def test_doxxkey_remascii_metadata_template_attr(self):
+        os.chdir('keys')  # switch to keys directory to test file load from inside the directory
+        key = DoxxKey(self.good_key_path_remtempl)
+        self.assertTrue('template' in key.meta_data.keys())  # assert that 'template' is one of keys in the meta_data attribute
+        self.assertEqual('http://test.com/dir/test.doxt', key.meta_data['template'])
+        
+        os.chdir(self.main_test_dir)
+    
+    ## Multiple remote template test
+    ## Single template unicode tests
+    ## Multi-template unicode tests
     ## Error tests
