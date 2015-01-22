b5b04b67edc405246c8e2a4fa5be25d5a44642d3
Chris Simpkins <git.simpkins@gmail.com>
2015-01-22 00:01:49 -0500

    new tests following the remote template refactor

--- tests/test_doxx-templates.py
+++ tests/test_doxx-templates.py
@@ -229,44 +229,55 @@ class DoxxRemoteTemplateTests(unittest.TestCase):
             self.assertEqual(str, type(temp.text))
             
 
-class DoxxTemplateErrors(unittest.TestCase):
+class DoxxTemplateErrorsTests(unittest.TestCase):
     
     def setUp(self):
+        self.good_template = "templates/ascii_template.doxt"
         self.bad_template_path = "templates/bogus.doxt"
+        self.bad_header = "templates/errors/bad_header.doxt"
         self.missing_extension = "templates/errors/missing_extension.doxt"
         self.missing_ext_val = "templates/errors/missing_extension_value.doxt"
         self.missing_template_text = "templates/errors/missing_template.doxt"
         self.undefined_dest_dir = "templates/errors/undefined_destination.doxt"
         self.undefined_basename = "templates/errors/undefined_basename.doxt"
         self.nonexist_url_template = "http://www.google.com/templates/doxx/doxx.doxt"
+        self.bad_url_form = "raw.githubusercontent.com/bit-store/testfiles/master/doxx/templates/ascii_template.doxt"
+        
+        self.ascii_standard = "standards/ascii_template_std.txt"
         
     
     # bad local file path
     def test_template_error_bad_local_path(self):
-        with self.assertRaises(SystemExit):
+        with self.assertRaises(IOError):
             temp = DoxxTemplate(self.bad_template_path)
             temp.load_data()
             temp.split_data()
             temp.parse_template_for_errors()
             temp.parse_template_text()
             
+    # properly formatted template file passes the parse_template_for_errors() tests
+    def test_template_errors_good_template(self):
+        temp = DoxxTemplate(self.good_template)
+        temp.load_data()
+        temp.split_data()
+        result = temp.parse_template_for_errors()
+        self.assertEqual(False, result[0])       
+    
     # malformed template file - absent header meta data section    
     def test_template_error_missing_header(self):
-        with self.assertRaises(SystemExit):
-            temp = DoxxTemplate(self.bad_template_path)
-            temp.load_data()
-            temp.split_data()
-            temp.parse_template_for_errors()
-            temp.parse_template_text()
+        temp = DoxxTemplate(self.bad_header)
+        temp.load_data()
+        temp.split_data()
+        result = temp.parse_template_for_errors()
+        self.assertEqual(True, result[0])  # first index of the tuple contains True/False for presence of template errors
             
     # malformed template file - missing extension field
     def test_template_error_missing_extension_field(self):
-        with self.assertRaises(SystemExit):
-            temp = DoxxTemplate(self.missing_extension)
-            temp.load_data()
-            temp.split_data()
-            temp.parse_template_for_errors()
-            temp.parse_template_text()
+        temp = DoxxTemplate(self.missing_extension)
+        temp.load_data()
+        temp.split_data()
+        result = temp.parse_template_for_errors()
+        self.assertEqual(True, result[0])  # the first index of the tuple contains True/False indicator
         
     def test_template_error_missing_extension_value(self):
         temp = DoxxTemplate(self.missing_ext_val)
@@ -296,16 +307,24 @@ class DoxxTemplateErrors(unittest.TestCase):
     
     # malformed template file - absent template data
     def test_template_error_missing_template_text(self):
-        with self.assertRaises(SystemExit):
-            temp = DoxxTemplate(self.missing_template_text)
+        temp = DoxxTemplate(self.missing_template_text)
+        temp.load_data()
+        temp.split_data()
+        result = temp.parse_template_for_errors()
+        self.assertEqual(True, result[0])
+    
+    # URL 404 error - nonexistent page
+    def test_template_error_http_status_404(self):
+        temp = RemoteDoxxTemplate(self.nonexist_url_template)
+        result = temp.load_data()
+        self.assertEqual(False, result[0])  # first index of the returned tuple contains True/False for success of HTTP load of template
+    
+    # malformed URL - attempt to recover by adding http:// but the github file only exists at a https:// URL, should provide user error message and sys.exit(1)
+    def test_template_error_http_bad_url_form(self):
+        with self.assertRaises(Exception):
+            temp = RemoteDoxxTemplate(self.bad_url_form)
             temp.load_data()
             temp.split_data()
             temp.parse_template_for_errors()
             temp.parse_template_text()
-    
-    # URL 404 error - nonexistent page
-    
-    # malformed URL
-    
-    
-    # URL timeout error - ? need to handle with try/except block    
+         
