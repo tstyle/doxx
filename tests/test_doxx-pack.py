eda72b3705219ac01a5d67c53772f64c69dd7d83
Chris Simpkins <git.simpkins@gmail.com>
2015-02-02 15:21:16 -0500

    new pack/unpack tests

--- tests/test_doxx-pack.py
+++ tests/test_doxx-pack.py
@@ -7,7 +7,7 @@ import unittest
 
 from Naked.toolshed.system import file_exists
 
-from doxx.commands.pack import tar_gzip_package_directory
+from doxx.commands.pack import tar_gzip_package_directory, zip_package_directory
 from doxx.commands.unpack import unpack_run, remove_compressed_archive_file
 
 class DoxxPackTests(unittest.TestCase):
@@ -16,12 +16,18 @@ class DoxxPackTests(unittest.TestCase):
         self.cwd = os.getcwd()
         self.pack_main_dir = "pack-unpack-tests"
         self.pack_dir = "pack-unpack-tests/pack"
-        self.unpack_dir = "pack-unpack-tests/unpack"
+        self.targz_unpack_dir = "pack-unpack-tests/unpack/targz"
+        self.zip_unpack_dir = "pack-unpack-tests/unpack/zip"
         
         # remove the test tar.gz file from the pack directory if present
         if file_exists("pack-unpack-tests/pack/packed.tar.gz"):
             os.remove("pack-unpack-tests/pack/packed.tar.gz")
             self.assertFalse(file_exists("pack-unpack-tests/pack/packed.tar.gz"))
+            
+        # remove the test .zip file from the pack directory if present
+        if file_exists("pack-unpack-tests/pack/packed.zip"):
+            os.remove("pack-unpack-tests/pack/packed.zip")
+            self.assertFalse(file_exists("pack-unpack-tests/pack/packed.zip"))        
             
         if file_exists('pack-unpack-tests/unpack/key.yaml'):
             os.remove('pack-unpack-tests/unpack/key.yaml')
@@ -35,17 +41,25 @@ class DoxxPackTests(unittest.TestCase):
             os.remove('pack-unpack-tests/unpack/test2.doxt')
             self.assertFalse(file_exists('pack-unpack-tests/unpack/test2.doxt'))
             
-        if not file_exists('pack-unpack-tests/unpack/unpack.tar.gz'):
+        if not file_exists('pack-unpack-tests/unpack/targz/unpack.tar.gz'):
             os.chdir('pack-unpack-tests')  # change to the pack/unpack test dir in order to create the new file
             tar_gzip_package_directory('unpack', 'pack')
             self.assertTrue(file_exists('unpack.tar.gz'))
-            os.rename('unpack.tar.gz', 'unpack/unpack.tar.gz')  # move it into the proper directory
-            self.assertTrue(file_exists('unpack/unpack.tar.gz'))
+            os.rename('unpack.tar.gz', 'unpack/targz/unpack.tar.gz')  # move it into the proper directory
+            self.assertTrue(file_exists('unpack/targz/unpack.tar.gz'))
+            os.chdir(self.cwd)
+            
+        if not file_exists('pack-unpack-tests/unpack/zip/unpack.zip'):
+            os.chdir('pack-unpack-tests')  # change to the pack/unpack test dir in order to create the new file
+            zip_package_directory('unpack', 'pack')
+            self.assertTrue(file_exists('unpack.zip'))
+            os.rename('unpack.zip', 'unpack/zip/unpack.zip')  # move it into the proper directory
+            self.assertTrue(file_exists('unpack/zip/unpack.zip'))
             os.chdir(self.cwd)
             
         
     # test the pack command
-    def test_doxx_pack_directory(self):
+    def test_doxx_targz_pack_directory(self):
         try:
             os.chdir(self.pack_main_dir)
             tar_gzip_package_directory("packed", "pack")
@@ -55,10 +69,20 @@ class DoxxPackTests(unittest.TestCase):
             os.chdir(self.cwd)
             raise e
         
-    # test the unpack command
-    def test_doxx_unpack_package(self):
+    # test zip pack command
+    def test_doxx_zip_pack_directory(self):
         try:
-            os.chdir(self.unpack_dir)
+            os.chdir(self.pack_main_dir)
+            zip_package_directory("packed", "pack")
+            self.assertTrue(file_exists('packed.zip'))
+            os.chdir(self.cwd)            
+        except Exception as e:
+            pass
+        
+    # test the unpack command with tar.gz file
+    def test_doxx_targz_unpack_package(self):
+        try:
+            os.chdir(self.targz_unpack_dir)
             unpack_run('unpack.tar.gz')
             self.assertTrue(file_exists('key.yaml'))
             self.assertTrue(file_exists('test.doxt'))
@@ -70,3 +94,19 @@ class DoxxPackTests(unittest.TestCase):
         except Exception as e:
             os.chdir(self.cwd)
             raise e
+        
+    # test the unpack command with zip file
+    def test_doxx_zip_unpack_package(self):
+        try:
+            os.chdir(self.zip_unpack_dir)
+            unpack_run('unpack.zip')
+            self.assertTrue(file_exists('key.yaml'))
+            self.assertTrue(file_exists('test.doxt'))
+            self.assertTrue(file_exists('test2.doxt'))
+            self.assertTrue(file_exists('unpack.zip'))
+            remove_compressed_archive_file('unpack.zip')
+            self.assertFalse(file_exists('unpack.zip'))
+            os.chdir(self.cwd)
+        except Exception as e:
+            os.chdir(self.cwd)
+            raise e        
