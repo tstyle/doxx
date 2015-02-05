c38f1d6fe316b37bae25b7019f14cc806430d841
Chris Simpkins <git.simpkins@gmail.com>
2015-02-05 15:30:19 -0500

    returns root directory when unpack archive file (necessary for project builds from archive files)

--- lib/doxx/commands/unpack.py
+++ lib/doxx/commands/unpack.py
@@ -8,25 +8,49 @@ from Naked.toolshed.system import stderr, file_exists
 
 def unpack_run(file_path):
     if tarfile.is_tarfile(file_path):
-        unpack_targz_archive_file(file_path)
+        return unpack_targz_archive_file(file_path)  # returns the root directory from the unpack function below
     elif zipfile.is_zipfile(file_path):
-        unpack_zip_archive_file(file_path)
+        return unpack_zip_archive_file(file_path)
     else:
         stderr("[!] doxx: '" + file_path + "' does not appear to be a supported project archive type.  Please review the project archive documentation and try again.", exit=1)
 
 def unpack_targz_archive_file(targz_file_path):
     try:
         tar = tarfile.open(targz_file_path, "r:*")
+        # define root directory in the tar archive to return to calling function
+        x = 0
+        root_dir = None
+        for tarinfo in tar:
+            if x == 0:
+                root_dir = tarinfo.name
+                x += 1
+            else:
+                break
+        # unpack the archive
         tar.extractall()
         tar.close()
+        # return the root directory to calling function
+        return root_dir
     except Exception as e:
         stderr("[!] doxx: Unable to unpack the file '" + targz_file_path + "'. Error: " + str(e))
         
 def unpack_zip_archive_file(zip_file_path):
     try:
         zip_archive = zipfile.ZipFile(zip_file_path, 'r')
+        # define root directory in the tar archive to return to calling function
+        x = 0
+        root_dir = None
+        for zipinfo in zip_archive.infolist():
+            if x == 0:
+                root_dir = zipinfo.filename
+                x += 1
+            else:
+                break
+        # extract the zip archive
         zip_archive.extractall()
         zip_archive.close()
+        # return the root directory in the zip archive
+        return root_dir
     except Exception as e:
         stderr("[!] doxx: Unable to unpack the file '" + zip_file_path + "'. Error: " + str(e))
         
