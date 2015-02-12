8f987368e7d049a7e1bba77b7b288c13fee9e835
Chris Simpkins <git.simpkins@gmail.com>
2015-02-11 22:19:16 -0500

    updates

--- lib/runner.py
+++ lib/runner.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from doxx.datatypes.template import DoxxTemplate, RemoteDoxxTemplate
+from doxx.utilities.fuzzysearch import FuzzySearcher
 
-dt = DoxxTemplate('/Users/ces/Desktop/code/doxx/tests/templates/ascii_template.doxt')
-dt.load_data()
-dt.split_data()
-dt.parse_template_for_errors()
-dt.parse_template_text()
-print('done')
+possibilities = ["license-mit", "license-gpl", "license-apache", "license-apacho", "html-initializr", "bogus-stuff", "another-tool", "meager-banish", "test-code", "test-code-thing", "test-code-longstring", "either-or", "project-good", "tapas-food", "pizza-beer", "coke-soda", "monster-blue", "longer-string-sample", "bad-string-sample-error", "big-sample-string-tester", "test-mit-license", "bogus-mit-string"]
+match_attempt = "sample string"
+
+print("Match String: '" + match_attempt + "'")
+print(" ")
+
+fuzzy = FuzzySearcher(match_attempt)
+for test_string in possibilities:
+    print(test_string + " : " + str(fuzzy.partial_set_ratio(test_string)))
+    
