bbdf3d78abc9502faa798a603bc560663b7df5cb
Chris Simpkins <git.simpkins@gmail.com>
2015-02-14 11:43:38 -0500

    updates

--- lib/runner.py
+++ lib/runner.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from doxx.datatypes.package import OfficialPackage
+import json
+from Naked.toolshed.network import HTTP
 
-test = OfficialPackage().get_package_key_url('license-apache')
-print(test)
+url = "https://api.github.com/orgs/doxx-repo/repos"
 
+http = HTTP(url)
+the_json = http.get()
+json_obj = json.loads(the_json)
 
-    
+repo_list = []
+
+for x in xrange(len(json_obj)):
+    repository_name = json_obj[x]['name']
+    if repository_name == "bin":
+        pass
+    elif repository_name == "master":
+        pass
+    else:
+        repo_list.append(repository_name)
+        
+print(sorted(repo_list))
