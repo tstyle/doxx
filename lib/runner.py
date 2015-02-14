d727c8a6096fdaa5a464d48fa3f1c7429e5fcb3d
Chris Simpkins <git.simpkins@gmail.com>
2015-02-13 23:19:31 -0500

    updates

--- lib/runner.py
+++ lib/runner.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from doxx.utilities.fuzzysearch import FuzzySearcher
+from doxx.datatypes.package import OfficialPackage
 
-possibilities = ["license-mit", "license-gpl", "license-apache", "license-apacho", "html-initializr", "bogus-stuff", "another-tool", "meager-banish", "test-code", "test-code-thing", "test-code-longstring", "either-or", "project-good", "tapas-food", "pizza-beer", "coke-soda", "monster-blue", "longer-string-sample", "bad-string-sample-error", "big-sample-string-tester", "test-mit-license", "bogus-mit-string"]
-match_attempt = "sample string"
+test = OfficialPackage().get_package_key_url('license-apache')
+print(test)
 
-print("Match String: '" + match_attempt + "'")
-print(" ")
 
-fuzzy = FuzzySearcher(match_attempt)
-for test_string in possibilities:
-    print(test_string + " : " + str(fuzzy.partial_set_ratio(test_string)))
     
