afd4c8c9acfc69138bc2eaf5937596b1de11aa71
Chris Simpkins <git.simpkins@gmail.com>
2015-02-14 11:43:25 -0500

    updated user error and stdout messages for pullkey

--- lib/doxx/commands/pullkey.py
+++ lib/doxx/commands/pullkey.py
@@ -3,6 +3,7 @@
 
 from Naked.toolshed.file import FileWriter
 from Naked.toolshed.network import HTTP
+from Naked.toolshed.system import stdout, stderr
 from doxx.datatypes.package import OfficialPackage
 
 ## TODO: change pulled key to include the remote repository path rather than local paths
@@ -12,6 +13,7 @@ def run_pullkey(package_name):
     package = OfficialPackage()
     key_file_url = package.get_package_key_url(normalized_package_name)
     try:
+        stdout("[*] doxx: Pulling remote key file...")
         http = HTTP(key_file_url)
         if http.get_status_ok():
             key_file_text =  http.res.text
@@ -20,7 +22,8 @@ def run_pullkey(package_name):
                 fr.write(key_file_text)
             except Exception as e:
                 stderr("[!] doxx: Unable to write the 'key.yaml' file to disk. Error: " + str(e), exit=1)
+            stdout("[*] doxx: Key file pull complete")
         else:
-            stderr("[!] doxx: Unable to pull the key file.  (HTTP status code: " + http.res.status_code + ")")
+            stderr("[!] doxx: Unable to pull the key file.  (HTTP status code: " + http.res.status_code + ")", exit=1)
     except Exception as e:
         stderr("[!] doxx: Unable to pull the key file. Error: " + str(e), exit=1)
