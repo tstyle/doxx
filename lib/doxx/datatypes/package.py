044ab919912c8b7586eb082901e9f1e4bdd8edd0
Chris Simpkins <git.simpkins@gmail.com>
2015-02-13 22:31:19 -0500

    updated paths to the package files

--- lib/doxx/datatypes/package.py
+++ lib/doxx/datatypes/package.py
@@ -5,20 +5,24 @@ from Naked.toolshed.file import FileReader
 
 class OfficialPackage(object):
     def __init__(self):
-        self.binary_package_url_prefix = "https://github.com/doxx-repo/bin/raw/master/"
-        self.package_key_file_url_prefix = "https://raw.githubusercontent.com/doxx-repo/"
-        self.package_key_file_url_postfix = "/master/pkey.yaml"
-        self.master_package_list_text_url = "http://doxx-repo.github.io/master/repository-list/list.txt"
+        self.binary_package_url_prefix = "https://github.com/doxx-repo/"
+        self.binary_package_url_postfix = "/releases/download/current/"
+        self.package_key_file_url_prefix = "https://github.com/doxx-repo/"
+        self.package_key_file_url_postfix = "/releases/download/current/pkey.yaml"
+        self.master_package_list_text_url = "https://objects.dreamhost.com/doxx/list.txt"
+        
+        # https://github.com/doxx-repo/license-apache/releases/download/current/license-apache.tar.gz
+        # https://github.com/doxx-repo/license-apache/releases/download/current/pkey.yaml
     
     # Getter methods
     
     def get_package_targz_url(self, package_name):
         normalized_package_name = package_name.lower().strip()
-        return self.binary_package_url_prefix + normalized_package_name + '.tar.gz'
+        return self.binary_package_url_prefix + normalized_package_name + self.binary_package_url_postfix + normalized_package_name + '.tar.gz'
     
     def get_package_zip_url(self, package_name):
         normalized_package_name = package_name.lower().strip()
-        return self.binary_package_url_prefix + normalized_package_name + '.zip'        
+        return self.binary_package_url_prefix + normalized_package_name + self.binary_package_url_postfix + normalized_package_name + '.zip'     
     
     def get_package_key_url(self, package_name):
         normalized_package_name = package_name.lower().strip()
