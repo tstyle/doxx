94cef3c9467af648314c1008c9bf34af941d7576
Chris Simpkins <git.simpkins@gmail.com>
2015-03-23 23:03:33 -0400

    added binary file and github repository pull code

--- lib/doxx/datatypes/github.py
+++ lib/doxx/datatypes/github.py
@@ -9,19 +9,19 @@ from Naked.toolshed.system import stderr, stdout, file_exists, dir_exists
 from doxx.commands.pull import pull_binary_file, pull_text_file
 from doxx.commands.unpack import unpack_run
 
-## TESTING
-import sys
 
 def pull_textfile_runner(text_url_dict):
     file_list = text_url_dict.keys()      # the local outfile names in a list    
     number_of_files = len(file_list)      # the number of files included in the list
-    if number_of_files > 0:   # multiple text file pull, each in separate process
-        if number_of_files > 1:
+    if number_of_files > 0:   
+        if number_of_files > 1:  # multiple text file pull, each in separate process
             processes = []       # list of spawned processes
             outputlock = Lock()  # stdout / stderr writes lock
+            iolock = Lock()      # input/output lock
             
+            # iterate through requested files and execute pull in separate process for each one
             for file_path in file_list:
-                p = Process(target=_pull_textfile_multiprocess, args=(file_path, text_url_dict[file_path], outputlock))
+                p = Process(target=_pull_textfile_multiprocess, args=(file_path, text_url_dict[file_path], outputlock, iolock))
                 p.start()
                 processes.append(p)
                 
@@ -32,33 +32,90 @@ def pull_textfile_runner(text_url_dict):
             file_path = file_list[0]
             _pull_textfile(file_path, text_url_dict[file_path])  # file_path is local path for write, dictionary value is the URL
     else:
-        pass
+        stderr("[!] doxx: Unable to find text files to pull in the key file", exit=0)
 
 def pull_binaryfile_runner(binary_url_dict):
-    pass
+    file_list = binary_url_dict.keys()      # the local outfile names in a list    
+    number_of_files = len(file_list)      # the number of files included in the list
+    if number_of_files > 0:   
+        if number_of_files > 1:  # multiple binary file pull, each in separate process
+            processes = []       # list of spawned processes
+            outputlock = Lock()  # stdout / stderr writes lock
+            iolock = Lock()      # input/output lock
+            
+            # iterate through requested files and execute pull in separate process for each one
+            for file_path in file_list:
+                p = Process(target=_pull_binaryfile_multiprocess, args=(file_path, binary_url_dict[file_path], outputlock, iolock))
+                p.start()
+                processes.append(p)
+                
+            for process in processes:
+                process.join(timeout=60)
+                
+        else:  # single text file pull
+            file_path = file_list[0]
+            _pull_binaryfile(file_path, binary_url_dict[file_path])  # file_path is local path for write, dictionary value is the URL
+    else:
+        stderr("[!] doxx: Unable to find binary files to pull in the key file", exit=0)
 
 def pull_github_repo_runner(repo_url_dict):
-    pass
+    file_list = repo_url_dict.keys()      # the local outfile names in a list    
+    number_of_files = len(file_list)      # the number of files included in the list
+    if number_of_files > 0:   
+        if number_of_files > 1:  # multiple binary file pull, each in separate process
+            stdout("[*] doxx: Hang in there. Pulling " + str(number_of_files) + " entire repositories. This may take a bit of time...")
+            processes = []       # list of spawned processes
+            outputlock = Lock()  # stdout / stderr writes lock
+            iolock = Lock()      # input/output lock
+            
+            # iterate through requested files and execute pull in separate process for each one
+            for file_path in file_list:
+                p = Process(target=_pull_github_repo_multiprocess, args=(file_path, repo_url_dict[file_path], outputlock, iolock))
+                p.start()
+                processes.append(p)
+                
+            for process in processes:
+                process.join(timeout=120)
+                
+        else:  # single text file pull
+            stdout("[*] doxx: Hang in there. Pulling an entire repository. This may take a bit of time...")
+            file_path = file_list[0]
+            _pull_github_repo(file_path, repo_url_dict[file_path])  # file_path is local path for write, dictionary value is the URL
+    else:
+        stderr("[!] doxx: Unable to find binary files to pull in the key file", exit=0)   
 
 def _pull_textfile(file_path, url):
     # create OS dependent file path (if necessary)
     file_path = _make_os_dependent_path(file_path)
     # make directory structure if necessary for the file path
-    _create_pull_dirs(file_path)
-    # pull the file and write to local filesystem    
-    pull_text_file(url, file_path)
+    if os.path.dirname(file_path) is not "":
+        _create_pull_dirs(file_path)
+    # pull the file and write to local filesystem
+    try:
+        pull_text_file(url, file_path)
+    except Exception as e:
+        stderr("[!] doxx: Unable to pull '" + file_path + "' from '" + url + "'. Error: " + str(e), exit=1)
+        
     if file_exists(file_path):
         stdout("[+] doxx: '" + file_path + "' ...check!")
     else:
         stderr("[!] doxx: There was an error pulling '" + file_path + "'. Error: Unable to locate local file.", exit=1)    
     
-def _pull_textfile_multiprocess(file_path, url, outputlock):
+def _pull_textfile_multiprocess(file_path, url, outputlock, iolock):
     # create OS dependent file path (if necessary)
     file_path = _make_os_dependent_path(file_path)
     # make directory structure if necessary for the file path
-    _create_pull_dirs(file_path)
+    if os.path.dirname(file_path) is not "":
+        iolock.acquire()
+        _create_pull_dirs(file_path)
+        iolock.release()
     # pull the file and write to local filesystem
-    pull_text_file(url, file_path)
+    try:
+        pull_text_file(url, file_path)
+    except Exception as e:
+        outputlock.acquire()
+        stderr("[!] doxx: Unable to pull '" + file_path + "' from '" + url + "'. Error: " + str(e), exit=0)
+        outputlock.release()
     if file_exists(file_path):
         outputlock.acquire()
         stdout("[+] doxx: '" + file_path + "' ...check!")
@@ -69,17 +126,88 @@ def _pull_textfile_multiprocess(file_path, url, outputlock):
         outputlock.release()
     
 def _pull_binaryfile(file_path, url):
-    pull_binary_file(url, file_path)
-    ## TODO: user messages
+    # create OS dependent file path (if necessary)
+    file_path = _make_os_dependent_path(file_path)
+    # make directory structure if necessary for the file path
+    if os.path.dirname(file_path) is not "":
+        _create_pull_dirs(file_path)
+    # pull the file and write to local filesystem    
+    try:
+        pull_binary_file(url, file_path)
+    except Exception as e:
+        stderr("[!] doxx: Unable to pull '" + file_path + "' from '" + url + "'. Error: " + str(e), exit=0)
+    if file_exists(file_path):
+        stdout("[+] doxx: '" + file_path + "' ...check!")
+    else:
+        stderr("[!] doxx: There was an error pulling '" + file_path + "'. Error: Unable to locate local file.", exit=1) 
     
 def _pull_binaryfile_multiprocess(file_path, url, outputlock):
-    pass
+    # create OS dependent file path (if necessary)
+    file_path = _make_os_dependent_path(file_path)
+    # make directory structure if necessary for the file path
+    if os.path.dirname(file_path) is not "":
+        iolock.acquire()
+        _create_pull_dirs(file_path)
+        iolock.release()
+    # pull the file and write to local filesystem
+    try:
+        pull_binary_file(url, file_path)
+    except Exception as e:
+        outputlock.acquire()
+        stderr("[!] doxx: Unable to pull '" + file_path + "' from '" + url + "'. Error: " + str(e), exit=0)
+        outputlock.release()
+    if file_exists(file_path):
+        outputlock.acquire()
+        stdout("[+] doxx: '" + file_path + "' ...check!")
+        outputlock.release()
+    else:
+        outputlock.acquire()
+        stderr("[!] doxx: There was an error pulling '" + file_path + "'. Error: Unable to locate local file", exit=0)
+        outputlock.release()
 
 def _pull_github_repo(file_path, url):
-    pass
+    # create OS dependent file path (if necessary)
+    file_path = _make_os_dependent_path(file_path)
+    # make directory structure if necessary for the file path
+    if os.path.dirname(file_path) is not "":
+        _create_pull_dirs(file_path)
+    # pull the file and write to local filesystem    
+    try:
+        pull_binary_file(url, file_path)
+    except Exception as e:
+        stderr("[!] doxx: Unable to pull the archive file from the URL '" + url + "'. Error: " + str(e), exit=0)
+    if file_exists(file_path):
+        root_dir = unpack_run(file_path)
+        remove(file_path)
+        stdout("[+] doxx: '" + root_dir + "' ...check!")
+    else:
+        stderr("[!] doxx: There was an error pulling the repository file. Error: Unable to locate local archive file.", exit=1)
 
-def _pull_github_repo_multiprocess(file_path, url, outputlock):
-    pass
+def _pull_github_repo_multiprocess(file_path, url, outputlock, iolock):
+    # create OS dependent file path (if necessary)
+    file_path = _make_os_dependent_path(file_path)
+    # make directory structure if necessary for the file path
+    if os.path.dirname(file_path) is not "":
+        iolock.acquire()
+        _create_pull_dirs(file_path)
+        iolock.release()
+    # pull the file and write to local filesystem    
+    try:
+        pull_binary_file(url, file_path)
+    except Exception as e:
+        outputlock.acquire()
+        stderr("[!] doxx: Unable to pull the archive file from the URL '" + url + "'. Error: " + str(e), exit=0)
+        outputlock.release()
+    if file_exists(file_path):
+        root_dir = unpack_run(file_path)
+        remove(file_path)
+        outputlock.acquire()
+        stdout("[+] doxx: '" + root_dir + "' ...check!")
+        outputlock.release()
+    else:
+        outputlock.acquire()
+        stderr("[!] doxx: There was an error pulling the repository file. Error: Unable to locate local archive file.", exit=1)
+        outputlock.release()
 
 def _make_os_dependent_path(file_path):
     """Makes an OS dependent file path from a POSIX file path"""
